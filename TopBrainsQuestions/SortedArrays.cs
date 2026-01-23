9bb024642681dab0b4d61438c3d82edaa013fc50
Nuthan-Reddy-Surukunti <nuthan.reddy@example.com>
2026-01-23 14:31:43 +0530

    Q 24 and 21 done

---
+++ TopBrainsQuestions/SortedArrays.cs
@@ -0,0 +1,50 @@
+class Program
+{
+    public static void Main()
+    {
+        int[] sortArr1 = {1,2,3,4};
+        int[] sortArr2 = {5,6,7,8};
+        int[] arr = SortArr(sortArr1,sortArr2);
+        foreach(int i in arr)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+    public static T[] SortArr<T>(T[] a,T[] b) where T : IComparable<T>
+    {
+        T[] result = new T[a.Length+b.Length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while (i < a.Length && j < b.Length)
+        {
+            if (a[i].CompareTo(b[j]) <= 0)
+            {
+                result[k]=a[i];
+                i++;
+            }
+            else
+            {
+                result[k]=b[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < a.Length)
+        {
+            result[k]=a[i];
+            i++;
+            k++;
+        }
+        while (j < b.Length)
+        {
+            result[k]=b[j];
+            j++;
+            k++;
+        }
+        return result;
+    }
+    
+}
+
---
+++ TopBrainsQuestions/StringFormat.cs
@@ -0,0 +1,28 @@
+using System.Text.Json;
+class Students
+{
+    public string Name { get; set; }
+    public int Marks { get; set; }
+}
+class Program
+{
+    public static void Main()
+    {
+        string[] strings = { "John:85", "Alice:92", "Bob:85", "David:70" };
+        int minScore =80;
+        List<Students> students = ToList(strings,minScore);
+        string json = JsonSerializer.Serialize(students);
+        Console.WriteLine(json);
+    }
+    public static List<Students> ToList(string[] input,int score)
+    {
+        List<Students> result = new List<Students>();
+        foreach(string i in input)
+        {
+            string[] parts = i.Split(":");
+            result.Add(new Students{Name=parts[0],Marks=Convert.ToInt32(parts[1])});
+        }
+        return result.Where(i=>i.Marks>=score).OrderByDescending(i=>i.Marks).ThenBy(i=>i.Name).ToList();
+    }
+    
+}
